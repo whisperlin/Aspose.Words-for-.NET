83ed802a3a563f905a362cb17b2801a577eaac3d
vyacheslav.deryushev <falleretic@gmail.com>
2018-07-04 19:42:40 +0700

    Rework sdcard path

--- ApiExamples/CSharp/ApiExamples.Xamarin.Android/ApiExampleBase.cs
+++ ApiExamples/CSharp/ApiExamples.Xamarin.Android/ApiExampleBase.cs
@@ -6,17 +6,11 @@
 //////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Android.App;
-using Android.Content;
-using Android.OS.Storage;
-using Android.Util;
 using Aspose.Words;
-using Java.Lang;
-using Java.Lang.Reflect;
 using NUnit.Framework;
 using Exception = System.Exception;
 using String = System.String;
@@ -133,44 +127,8 @@ namespace ApiExamples
             get { return gGoldsDir; }
         }
 
-        static Android.OS.Storage.StorageManager GetStorageManager()
-        {
-            return (StorageManager)Application.Context.ApplicationContext.GetSystemService(Context.StorageService);
-        }
-
         static ApiExampleBase()
         {
-            GetExternalSdCardPath();
-
-            List<String> allPaths = new List<String>();
-            try
-            {
-                Class storageVolumeClass = Class.ForName("android.os.storage.StorageVolume");
-                Method getVolumeList = GetStorageManager().Class.GetMethod("getVolumeList");
-                Method getPath = storageVolumeClass.GetMethod("getPath");
-                Method getState = storageVolumeClass.GetMethod("getState");
-                Java.Lang.Object getVolumeResult = getVolumeList.Invoke(GetStorageManager());
-                int length = Java.Lang.Reflect.Array.GetLength(getVolumeResult);
-
-                for (int i = 0; i < length; i++)
-                {
-                    Java.Lang.Object storageVolumeElem = Java.Lang.Reflect.Array.Get(getVolumeResult, i);
-                    String mountStatus = (String) getState.Invoke(storageVolumeElem);
-                    if (mountStatus != null && mountStatus.Equals("mounted"))
-                    {
-                        String path = (String) getPath.Invoke(storageVolumeElem);
-                        if (path != null)
-                        {
-                            allPaths.Add(path);
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                var eStackTrace = e.StackTrace;
-            }
-
             gArtifactsDir = Path.Combine(mExternalAppPath, "Data/Artifacts/");
             gMyDir = Path.Combine(mExternalAppPath, "Data/");
             gImageDir = Path.Combine(mExternalAppPath, "Data/Images/");
@@ -188,7 +146,7 @@ namespace ApiExamples
 
         public void CopyData()
         {
-            string sdCardDataDir = "/mnt/media_rw/1018-1B1C/Data/";
+            string sdCardDestinationPath = mSdCardPath + "Data/";
             string dataDestinationPath = mExternalAppPath + "Data/";
 
             if (Directory.Exists(dataDestinationPath))
@@ -202,10 +160,10 @@ namespace ApiExamples
             }
 
             //Now Create all of the directories
-            foreach (string dirPath in Directory.GetDirectories(sdCardDataDir, "*",
+            foreach (string dirPath in Directory.GetDirectories(sdCardDestinationPath, "*",
                 SearchOption.AllDirectories))
             {
-                Directory.CreateDirectory(dirPath.Replace(sdCardDataDir, dataDestinationPath));
+                Directory.CreateDirectory(dirPath.Replace(sdCardDestinationPath, dataDestinationPath));
 
                 string test = ExtractString(@"\w+$", dirPath);
 
@@ -217,9 +175,9 @@ namespace ApiExamples
             }
 
             //Copy all the files & Replaces any files with the same name
-            foreach (string newPath in Directory.GetFiles(sdCardDataDir, "*.*",
+            foreach (string newPath in Directory.GetFiles(sdCardDestinationPath, "*.*",
                 SearchOption.AllDirectories))
-                File.Copy(newPath, newPath.Replace(sdCardDataDir, dataDestinationPath), true);
+                File.Copy(newPath, newPath.Replace(sdCardDestinationPath, dataDestinationPath), true);
         }
 
         /// <summary>
@@ -287,131 +245,6 @@ namespace ApiExamples
             Match match = pattern.Match(searchString);
 
             return match.Value;
-        }
-
-        /// <summary>
-        /// Remember to turn on the READ_EXTERNAL_STORAGE permission, or this just comes back empty
-        /// Tries to establish whether there's an external SD card present. It's
-        /// a little hacky; reads /proc/mounts and looks for /storage/ references,
-        /// and iterates over those looking for things like 'ext' and 'sdcard' in
-        /// the same line, e.g. /storage/extSdCard or /storage/externalSd or similar.
-        /// For the moment, the existence of 'ext' as part of the path (not the file system type) 
-        /// is a crucial flag. If it doesn't see that, it'll bail out and assume there 
-        /// isn't one (even if there is and it's  named something else). 
-        /// We may have to build a list over time. 
-        /// Returns: The root of the mounted directory (with no trailing '/', or empty string if there isn't one)
-        /// </summary>
-        /// <returns>The root of the mounted directory (with NO trailing '/'), or empty string if there isn't one)</returns>
-        public static string GetExternalSdCardPath()
-        {
-            string procMounts = ReadProcMounts();
-            string sdCardEntry = ParseProcMounts(procMounts);
-
-            // note that IsWritable may fail if the disk is mounted elsewhere, e.g. MTP to PC
-            if (!string.IsNullOrWhiteSpace(sdCardEntry))
-            {
-                return sdCardEntry;
-            }
-            return string.Empty;
-        }
-
-        /// <summary>
-        /// Just returns the contents of /proc/mounts as a string.
-        /// Note that you MAY need to wrap this call up in a try/catch if you
-        /// anticipate permissions issues, but generally just reading from 
-        /// this file is OK
-        /// </summary>
-        /// <returns></returns>
-        public static string GetProcMountsContents()
-        {
-            return ReadProcMounts();
-        }
-
-        /// <summary>
-        /// This is an expensive operation to call, because it physically tries to write to the media.
-        /// Remember to turn on the WRITE_EXTERNAL_STORAGE permission, or this will always return false.
-        /// </summary>
-        /// <param name="pathToTest">The root path of the alleged SD card (e.g. /storage/externalSd), 
-        /// or anywhere else you want to test (WITHOUT the trailing '/'). If you try to write to somewhere 
-        /// you're not allowed to, you may get eaten by a dragon.</param>
-        /// <returns>True if it could write to it, false if not</returns>
-        public static bool IsWritable(string pathToTest)
-        {
-            bool result = false;
-
-            if (!string.IsNullOrWhiteSpace(pathToTest))
-            {
-                const string someTestText = "some test text";
-                try
-                {
-                    string testFile = string.Format("{0}/{1}.txt", pathToTest, Guid.NewGuid());
-                    Log.Info("ExternalSDStorageHelper", "Trying to write some test data to {0}", testFile);
-                    System.IO.File.WriteAllText(testFile, someTestText);
-                    Log.Info("ExternalSDStorageHelper", "Success writing some test data to {0}!", testFile);
-                    System.IO.File.Delete(testFile);
-                    Log.Info("ExternalSDStorageHelper", "Cleaned up test data file {0}", testFile);
-                    result = true;
-                }
-                catch (Exception ex) // shut up about it and move on, we obviously can't have it, so it's dead to us, we can't use it.
-                {
-                    Log.Error("ExternalSDStorageHelper", string.Format("Exception: {0}\r\nMessage: {1}\r\nStack Trace: {2}", ex, ex.Message, ex.StackTrace));
-                }
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// example entries from /proc/mounts on a Samsung Galaxy S2 looks like:
-        /// dev/block/dm-1 /mnt/asec/com.touchtype.swiftkey-2 ext4 ro,dirsync,nosuid,nodev,blah
-        /// dev/block/dm-2 /mnt/asec/com.mobisystems.editor.office_registered-2 ext4 ro,dirsync,nosuid, blah
-        /// dev/block/vold/259:3 /storage/sdcard0 vfat rw,dirsync, blah (this is NOT an external SD card)
-        /// dev/block/vold/179:9 /storage/extSdCard vfat rw,dirsync,nosuid, blah (this IS an external SD card)
-        /// </summary>
-        /// <param name="procMounts"></param>
-        /// <returns></returns>
-        private static string ParseProcMounts(string procMounts)
-        {
-            string sdCardEntry = string.Empty;
-            if (!string.IsNullOrWhiteSpace(procMounts))
-            {
-                var candidateProcMountEntries = procMounts.Split('\n', '\r').ToList();
-                candidateProcMountEntries.RemoveAll(s => s.IndexOf("storage", StringComparison.OrdinalIgnoreCase) < 0);
-                var bestCandidate = candidateProcMountEntries
-                  .FirstOrDefault(s => s.IndexOf("ext", StringComparison.OrdinalIgnoreCase) >= 0
-                                       && s.IndexOf("sd", StringComparison.OrdinalIgnoreCase) >= 0
-                                       && s.IndexOf("fat", StringComparison.OrdinalIgnoreCase) >= 0); // you can have things like fat, vfat, exfat, texfat, etc.
-
-                // e.g. /dev/block/vold/179:9 /storage/extSdCard vfat rw,dirsync,nosuid, blah
-                if (!string.IsNullOrWhiteSpace(bestCandidate))
-                {
-                    var sdCardEntries = bestCandidate.Split(' ');
-                    sdCardEntry = sdCardEntries.FirstOrDefault(s => s.IndexOf("/storage/", System.StringComparison.OrdinalIgnoreCase) >= 0);
-                    return !string.IsNullOrWhiteSpace(sdCardEntry) ? string.Format("{0}", sdCardEntry) : string.Empty;
-                }
-            }
-            return sdCardEntry;
-        }
-
-        /// <summary>
-        /// This doesn't require you to add any permissions in your Manifest.xml, but you'll
-        /// need to add READ_EXTERNAL_STORAGE at the very least to be able to determine if the external
-        /// SD card is available and usable.
-        /// </summary>
-        /// <returns></returns>
-        private static string ReadProcMounts()
-        {
-            Log.Info("ExternalSDStorageHelper", "Attempting to read '/proc/mounts' to see if there's an external SD card reference");
-            try
-            {
-                string contents = System.IO.File.ReadAllText("/proc/mounts");
-                return contents;
-            }
-            catch (Exception ex) // shut up about it and move on, we obviously can't have it, we can't use it.
-            {
-                Log.Error("ExternalSDStorageHelper", string.Format("Exception: {0}\r\nMessage: {1}\r\nStack Trace: {2}", ex, ex.Message, ex.StackTrace));
-            }
-
-            return string.Empty; // expect to fail by default
         }
     }
 }
